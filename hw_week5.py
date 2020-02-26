99658ae3d579b6176c9e48541af67f905e765ff8
AllenLyu <a314007@gmail.com>
2020-02-26 16:12:27 +0800

    update hw_week5

--- hw_week5.py
+++ hw_week5.py
@@ -1,9 +1,7 @@
 def hw_week5():
-
-
-for  x  in range(1, 10 , 1)
-    for y in range (1, 10, 1)
-    print(str(x) + 'x' + str(y) + '=' +str(x*y))
+  for  x  in range(1, 10 , 1):
+    for  y  in range (1, 10, 1):
+      print(str(x) + 'x' + str(y) + '=' +str(x*y))
 
 if __name__ == '__main__':
     hw_week5()
