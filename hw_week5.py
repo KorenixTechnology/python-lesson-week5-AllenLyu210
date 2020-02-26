384dad8ad0fa1f049987e2577842c8d2781b58cc
AllenLyu <a314007@gmail.com>
2020-02-26 16:03:45 +0800

    homework_week5

--- hw_week5.py
+++ hw_week5.py
@@ -1,7 +1,9 @@
 def hw_week5():
-    # Your code here
-    pass
 
+
+for  x  in range(1, 10 , 1)
+    for y in range (1, 10, 1)
+    print(str(x) + 'x' + str(y) + '=' +str(x*y))
 
 if __name__ == '__main__':
     hw_week5()
